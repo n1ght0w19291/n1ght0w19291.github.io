24039b4174e3319b74ca1940c52fa8c052161811
Huang Pin-Chen <116648694+n1ght0w19291@users.noreply.github.com>
2025-08-27 18:24:38 +0800

    Update site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://n1ght0w19291.github.io/",
   base: "/",
   trailingSlash: "always",
   integrations: [
